ad6942eba70fa10df576728b4ef4dbe0be2a0b36
yyb <1064804095@qq.com>
2021-07-08 22:26:46 +0800

    测试类中调用其他类的属性

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
 import numpy as np
+from test1 import Tree
 
 test = np.array([1,2,3,4,5])
 
+class yyb:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.lr = 0.05
+    def fit(self, val):
+        model = Tree(self.x, self.y, obj=self)
+        tmp = model.build_tree(val)
+        print(tmp)
+
 if __name__ == '__main__':
-    print(test)
+    y1 = yyb(1, 2)
+    res = y1.fit(val=2)
+    print(res)
--- test1.py
+++ test1.py
@@ -5,5 +5,17 @@ test = pd.DataFrame({'id':['1', '2', '3', '4'], 'value':[54, 56, 87, 21]})
 
 test['value'] = np.round(test['value']/3, 2)
 
+class Tree(object):
+    def __init__(self, x, y, obj):
+        self.x = x
+        self.y = y
+        self.obj = obj
+
+    def build_tree(self, m):
+        res = self.x + self.y + m + self.obj.lr
+        return res
+
 if __name__ == '__main__':
-    print(test)
+    tre = Tree(1, 2)
+    res = tre.build_tree(3)
+    print(res)
